7ee87744a9deb73b4617d676fdd9811c2789f3c1
Ruslan Atarov <ruslanatarov@gmail.com>
2023-07-05 15:57:28 +0300

    fix

--- play_board/play_board/settings.py
+++ play_board/play_board/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.sites',
+    'django.contrib.sites',
 
     'sorl.thumbnail',
     'debug_toolbar',
